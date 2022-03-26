54d1574f8494dcc3c753190f568824c9aa242607
james <judilsteve>
2022-03-26 20:19:08 +0800

    Bare Metal

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSystemd();
+
+builder.WebHost.ConfigureKestrel(ko => {
+    ko.UseSystemd();
+});
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Yo");
+
+await app.RunAsync();
